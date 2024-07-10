26e0911cfa9cc4eb09d907a774d5ffe209d7bcc3
Jichani <123962013+jichani@users.noreply.github.com>
2024-07-10 09:18:02 +0900

    enum 타입 추가

---
+++ section2/src/chapter5.ts
@@ -0,0 +1,37 @@
+// enum 타입
+// 여러가지 값들에 각각 이름을 부여해 열거해두고 사용하는 타입
+
+enum Role {
+    ADMIN, // 0 할당(자동)
+    USER, // 1 할당(자동)
+    GUEST, // 2 할당(자동)
+}
+
+const user1 = {
+    name: '이정환',
+    role: Role.ADMIN, // 0
+};
+
+const user2 = {
+    name: '홍길동',
+    role: Role.USER, // 1
+};
+
+const user3 = {
+    name: '아무개',
+    role: Role.GUEST, // 2
+};
+
+// 문자열 열거형
+enum Language {
+    korean = 'ko',
+    english = 'en',
+}
+
+const user4 = {
+    name: '이정환',
+    role: Role.ADMIN, // 0
+    language: Language.korean, // "ko"
+};
+
+// enum은 컴파일 결과 객체가 된다.
